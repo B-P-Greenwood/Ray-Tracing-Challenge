2f8c9fcc554a592e127898f9d7fe5e521832719c
BPGreenwood <53783989+B-P-Greenwood@users.noreply.github.com>
2022-10-08 18:30:33 +0100

    Completed Chapter 1

--- index.js
+++ index.js
@@ -33,3 +33,87 @@ export function minusTuples(a, b) {
   }
   return result;
 }
+
+export function negate(tuple) {
+  let result = [];
+  tuple.forEach((item) => {
+    result.push(-item);
+  });
+  return result;
+}
+
+export function scalar(tuple, scalar) {
+  let result = [];
+  tuple.forEach((item) => {
+    result.push(item * scalar);
+  });
+  return result;
+}
+
+export function magnitude(vector) {
+  let reduced = 0;
+  vector.forEach((item) => {
+    reduced += item * item;
+  });
+  return Math.sqrt(reduced);
+}
+export function normalise(tuple) {
+  let result = [];
+  const mag = magnitude(tuple);
+  tuple.forEach((item) => {
+    result.push(item / mag);
+  });
+  return result;
+}
+export function dotProduct(a, b) {
+  let result = 0;
+  a.forEach((item, index) => {
+    result += item * b[index];
+  });
+  return result;
+}
+export function crossProduct(a, b) {
+  // x = 0;
+  // y = 1;
+  // z = 2;
+  let crossX = a[1] * b[2] - a[2] * b[1];
+  let crossY = a[2] * b[0] - a[0] * b[2];
+  let crossZ = a[0] * b[1] - a[1] * b[0];
+  return vector(crossX, crossY, crossZ);
+}
+
+export function tick(env, proj) {
+  let position = [];
+  let velocity = [];
+  proj.position.forEach((item, index) => {
+    position.push(item + proj.velocity[index]);
+  });
+
+  proj.velocity.forEach((item, index) => {
+    velocity.push(item + env.gravity[index] + env.wind[index]);
+  });
+  return new Projectile(position, velocity);
+}
+
+class Projectile {
+  constructor(position, velocity) {
+    this.position = position; //point
+    this.velocity = velocity; //vector
+  }
+}
+
+class Environment {
+  constructor(gravity, wind) {
+    this.gravity = gravity; //vector
+    this.wind = wind; //vector
+  }
+}
+let p = new Projectile(point(0, 1, 0), normalise(vector(4, 2, 0)));
+let e = new Environment(vector(0, -0.1, 0), vector(-0.01, 0, 0));
+
+let ticks = 0;
+while (p.position[1] >= 0) {
+  p = tick(e, p);
+  ticks++;
+  console.log('position: ', p.position, 'number of ticks :', ticks);
+}
--- index.test.js
+++ index.test.js
@@ -6,13 +6,19 @@ import {
   equal,
   addTuples,
   minusTuples,
+  negate,
+  scalar,
+  magnitude,
+  normalise,
+  dotProduct,
+  crossProduct,
 } from './index.js';
 
 describe('Calling tuple(4.3, -4.2, 3.1,1), returns tuple a with following attributes...', function () {
   const outcome = [4.3, -4.2, 3.1, 1];
   const actual = tuple(4.3, -4.2, 3.1, 1);
   test('Tuple a equals [4.3, -4.2, 3.1, 1]', function () {
-    expect(actual).toStrictEqual(outcome);
+    expect(outcome).toStrictEqual(actual);
   });
   test('a.x = 4.3', function () {
     expect(actual[0]).toStrictEqual(outcome[0]);
@@ -29,7 +35,7 @@ describe('Calling tuple(4.3, -4.2, 3.1,1), returns tuple a with following attrib
   test('a is a point (w=1)', function () {
     expect(actual[3]).toStrictEqual(1);
   });
-  test('a is not a vector (w!=0)', function () {
+  test('a is not a vector (w!=1)', function () {
     expect(actual[3]).not.toStrictEqual(0);
   });
 });
@@ -52,10 +58,10 @@ describe('Calling tuple(4.3, -4.2, 3.1, 0), returns tuple a with following attri
   test('a.w = 1.0', function () {
     expect(actual[3]).toStrictEqual(outcome[3]);
   });
-  test('a is a vector (w=1)', function () {
+  test('a is a vector (w=0)', function () {
     expect(actual[3]).toStrictEqual(0);
   });
-  test('a is not a point (w!=0)', function () {
+  test('a is not a point (w!=1)', function () {
     expect(actual[3]).not.toStrictEqual(1);
   });
 });
@@ -82,8 +88,8 @@ describe('Test to confirm adding two tuples is correct', function () {
   test('tuple(3,-2,5,1)+tuple(-2,3,1,0) = tuple(1,1,6,1)', function () {
     const a1 = tuple(3, -2, 5, 1);
     const a2 = tuple(-2, 3, 1, 0);
-    const outcome = addTuples(a1, a2);
-    const actual = tuple(1, 1, 6, 1);
+    const actual = addTuples(a1, a2);
+    const outcome = tuple(1, 1, 6, 1);
     expect(actual).toStrictEqual(outcome);
   });
 });
@@ -91,8 +97,8 @@ describe('Test to confirm subtracting two points is the correct vector', functio
   test('point(3,2,1)-point(5,6,7) = vector(-2,-4,-6)', function () {
     const a1 = point(3, 2, 1);
     const a2 = point(5, 6, 7);
-    const outcome = minusTuples(a1, a2);
-    const actual = vector(-2, -4, -6);
+    const actual = minusTuples(a1, a2);
+    const outcome = vector(-2, -4, -6);
     expect(actual).toStrictEqual(outcome);
   });
 });
@@ -100,8 +106,8 @@ describe('Test to confirm subtracting a vector from a point gives the correct po
   test('point(3,2,1)-vector(5,6,7) = point(-2,-4,-6)', function () {
     const a1 = point(3, 2, 1);
     const a2 = vector(5, 6, 7);
-    const outcome = minusTuples(a1, a2);
-    const actual = point(-2, -4, -6);
+    const actual = minusTuples(a1, a2);
+    const outcome = point(-2, -4, -6);
     expect(actual).toStrictEqual(outcome);
   });
 });
@@ -109,8 +115,116 @@ describe('Test to confirm subtracting a vector from a vector gives the correct v
   test('vector(3,2,1)-vector(5,6,7) = vector(-2,-4,-6)', function () {
     const a1 = vector(3, 2, 1);
     const a2 = vector(5, 6, 7);
-    const outcome = minusTuples(a1, a2);
-    const actual = vector(-2, -4, -6);
+    const actual = minusTuples(a1, a2);
+    const outcome = vector(-2, -4, -6);
+    expect(actual).toStrictEqual(outcome);
+  });
+});
+describe('Tests to confirm that subtracting a vector from a zero vector is correct', function () {
+  test('vector(1,-2,3) - vector(0,0,0) = vector(-1,2,-3)', function () {
+    const a1 = vector(1, -2, 3);
+    const a2 = vector(0, 0, 0);
+    const actual = minusTuples(a2, a1);
+    const outcome = vector(-1, 2, -3);
+    expect(actual).toStrictEqual(outcome);
+  });
+  test('negating tuple(1,-2,3,-4) = vector(-1,2,-3,4)', function () {
+    const a1 = tuple(1, -2, 3, -4);
+    const actual = negate(a1);
+    const outcome = tuple(-1, 2, -3, 4);
+    expect(actual).toStrictEqual(outcome);
+  });
+});
+describe('Tests to confirm tuple scaling', function () {
+  test('Multiply tuple(1,-2,3,-4) by 3.5 = tuple(3.5,-7,10.5,-14)', function () {
+    const a1 = tuple(1, -2, 3, -4);
+    const actual = scalar(a1, 3.5);
+    const outcome = tuple(3.5, -7, 10.5, -14);
+    expect(actual).toStrictEqual(outcome);
+  });
+  test('Divide tuple(1,-2,3,-4) by 2 = tuple(0.5,-1,1.5,-2)', function () {
+    const a1 = tuple(1, -2, 3, -4);
+    const actual = scalar(a1, 0.5);
+    const outcome = tuple(0.5, -1, 1.5, -2);
+    expect(actual).toStrictEqual(outcome);
+  });
+});
+describe('Tests to confirm the magnitude of vectors', function () {
+  test('The magnitude of vector(1,0,0) is 1', function () {
+    const a1 = vector(1, 0, 0);
+    const actual = magnitude(a1);
+    const outcome = 1;
+    expect(actual).toStrictEqual(outcome);
+  });
+  test('The magnitude of vector(0,1,0) is 1', function () {
+    const a1 = vector(0, 1, 0);
+    const actual = magnitude(a1);
+    const outcome = 1;
+    expect(actual).toStrictEqual(outcome);
+  });
+  test('The magnitude of vector(0,0,1) is 1', function () {
+    const a1 = vector(0, 0, 1);
+    const actual = magnitude(a1);
+    const outcome = 1;
+    expect(actual).toStrictEqual(outcome);
+  });
+  test('The magnitude of vector(1,2,3) is sqrt14', function () {
+    const a1 = vector(1, 2, 3);
+    const actual = magnitude(a1);
+    const outcome = Math.sqrt(14);
+    expect(actual).toStrictEqual(outcome);
+  });
+  test('The magnitude of vector(-1,-2,-3) is sqrt14', function () {
+    const a1 = vector(-1, -2, -3);
+    const actual = magnitude(a1);
+    const outcome = Math.sqrt(14);
+    expect(actual).toStrictEqual(outcome);
+  });
+});
+describe('Tests to confirm the normalisation of a vector', function () {
+  test('Vector(4,0,0) normalised is vector(1,0,0)', function () {
+    const a1 = vector(4, 0, 0);
+    const actual = normalise(a1);
+    const outcome = vector(1, 0, 0);
+    expect(actual).toStrictEqual(outcome);
+  });
+  test('Vector(1,2,3) normalised is vector(1/sqrt14, 2/sqrt14, 3/sqrt14)', function () {
+    const a1 = vector(1, 2, 3);
+    const actual = normalise(a1);
+    const sqrt = Math.sqrt(14);
+    const outcome = vector(1 / sqrt, 2 / sqrt, 3 / sqrt);
+    expect(actual).toStrictEqual(outcome);
+  });
+  test('The magnitude of a normalised vector(1,2,3) is 1)', function () {
+    const a1 = vector(1, 2, 3);
+    const norm = normalise(a1);
+    const actual = magnitude(norm);
+    const outcome = 1;
+    expect(actual).toStrictEqual(outcome);
+  });
+});
+describe('Test for the dot product of two vectors', function () {
+  test('Vector(1,2,3) and vector(2,3,4) have a dot product of 20', function () {
+    const a1 = vector(1, 2, 3);
+    const a2 = vector(2, 3, 4);
+    const actual = dotProduct(a1, a2);
+    const outcome = 20;
+    expect(actual).toStrictEqual(outcome);
+  });
+});
+describe('Test for the cross product of two vectors', function () {
+  test('vector(1,2,3) and vector(2,3,4) have a cross product of vector(-1,2,-1)', function () {
+    const a1 = vector(1, 2, 3);
+    const a2 = vector(2, 3, 4);
+    const actual = crossProduct(a1, a2);
+    const outcome = vector(-1, 2, -1);
+    expect(actual).toStrictEqual(outcome);
+  });
+  test('vector(1,2,3) and vector(2,3,4) have a cross product of vector(-1,2,-1)', function () {
+    const a1 = vector(1, 2, 3);
+    const a2 = vector(2, 3, 4);
+    const actual = crossProduct(a2, a1);
+    const outcome = vector(1, -2, 1);
     expect(actual).toStrictEqual(outcome);
   });
 });
